11a5a9dc31ddeb2b0c0c1b6d8ccb62e42940856c
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-11-14 04:02:23 +0100

    Added Selector condition class Restructured project.

---
+++ Assets/BehaviorTree/BehaviourTreeComponenets/SelectorCondition.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+/// <summary>
+/// Used by selector class
+/// </summary>
+public class SelectorCondition
+{
+    private Func<bool> m_condition;
+
+    /// <summary>
+    /// Load a function as a condition
+    /// </summary>
+    /// <param name="condition"></param>
+    public void LoadCondition(Func<bool> condition)
+    {
+        m_condition = condition;
+    }
+
+    /// <summary>
+    /// Evaluate loaded condition as routine state.
+    /// </summary>
+    /// <returns></returns>
+    public RoutineState Evaluate()
+    {
+        return m_condition.Invoke() ? RoutineState.Succeded : RoutineState.Failed;
+    }
+}
