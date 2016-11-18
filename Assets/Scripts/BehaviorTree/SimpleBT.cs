90d66544fef44106620c0be4273642e00262862d
Alex Busnita <alexandru.busnita@yahoo.ro>
2016-11-18 23:10:11 +0100

    Move simple BT Took bt/needs out of WolfBehavior Added navmesh obstacles in the scene Restructured wolf rest area

---
+++ Assets/Scripts/BehaviorTree/SimpleBT.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+
+public class SimpleBT : MonoBehaviour {
+
+    //private int switchTarget = 0;
+    private GameManager.WolfState lastState = GameManager.WolfState.Idle;
+
+    //private void Update() {
+    //    if (Input.GetKeyDown(KeyCode.H)) {
+    //        //when finished will run constantly
+    //        Debug.Log("Running BT");
+    //        RunBehaviorTree();
+    //    }
+    //}
+
+    //public string GetTarget() {
+    //    switch (switchTarget) {
+    //        case 1:
+    //            return GameManager.TagName.Water.ToString();
+    //        case 2:
+    //            return GameManager.TagName.Food.ToString();
+    //        case 3:
+    //            return GameManager.TagName.RestArea.ToString();
+    //        case 4:
+    //            return GameManager.TagName.None.ToString();
+    //        case 5:
+    //            return GameManager.TagName.Wolf.ToString();
+    //        default:
+    //            return null;
+    //    }
+    //}
+
+    public GameManager.WolfState GetWolfState() {
+        return lastState;
+    }
+
+    //private void RunBehaviorTree() {
+    //    {
+    //        Needs currNeeds = GetComponent<Needs>();
+
+    //        //fake way to make it work
+    //        if (currNeeds.GetNeed(GameManager.NeedType.Energy) < 50f) {
+    //            if (lastState != GameManager.WolfState.Run && lastState != GameManager.WolfState.Sleep) {
+    //                lastState = GameManager.WolfState.Run;
+    //                switchTarget = 3;
+    //            } else if (GetComponent<WolfBehavoir>().m_currLocation == GameManager.TagName.RestArea.ToString()) {
+    //                lastState = GameManager.WolfState.Sleep;
+    //                switchTarget = 4;
+    //            }
+    //        } else if (currNeeds.GetNeed(GameManager.NeedType.Thirst) > 40f && lastState != GameManager.WolfState.Drink) {
+    //            //supposedly the wolf first goes to the place and upon arrival enters drink state
+    //            lastState = GameManager.WolfState.Drink;
+    //            switchTarget = 1;
+    //        } else if (currNeeds.GetNeed(GameManager.NeedType.Hunger) > 40f && lastState != GameManager.WolfState.Feed) {
+    //            //supposedly the wolf first goes to the place and upon arrival enters eat state
+    //            lastState = GameManager.WolfState.Feed;
+    //            switchTarget = 2;
+    //        } else if (currNeeds.GetNeed(GameManager.NeedType.Energy) > 30 &&
+    //                   currNeeds.GetNeed(GameManager.NeedType.Playfulness) > 30 &&
+    //                   lastState != GameManager.WolfState.Play) {
+    //            //supposedly the wolf first goes to the wolf and upon arrival enters play state
+    //            lastState = GameManager.WolfState.Play;
+    //            switchTarget = 5;
+    //        } else {
+    //            lastState = GameManager.WolfState.Idle;
+    //            switchTarget = 0;
+    //        }
+    //    }
+
+    //    //the real way it will work
+    //    //switchTarget = btBuilder.ExecuteTree(GetComponent<Needs>());    -- baaakkaaa, it won't work like that ^^
+    //}
+}
--- Assets/Scripts/Wolf/WolfBehavoir.cs
+++ Assets/Scripts/Wolf/WolfBehavoir.cs
@@ -4,20 +4,38 @@ using System.Collections.Generic;
 
 public class WolfBehavoir : MonoBehaviour {
     
-    public string m_currLocation;
-
-    [Space(10)]
     [SerializeField]
     private AIDetectionModule m_detectionModule;
     [SerializeField]
     private AIMovementModule m_movementModule;
     [SerializeField]
     private AIMemoryModule m_memoryModule;
+    public bool hasPack;
 
-    private string m_TargetTag = "";
-    private GameObject m_Target;
-    private List<GameObject> memorizedObjects;
-    private SimpleBT m_Behavior;
+
+
+    BaseRoutine CreateBehavoirTree()
+    {
+
+        BehaviorTreeBuilder treeBuilder = new BehaviorTreeBuilder();
+
+
+
+        treeBuilder.BeginSelector("Initial State Selector")
+            .BeginCondition("Non-Pack Behavior", () => { return hasPack; })
+            .AttachTree(null)
+            .FinishNode()
+            .FinishNode();
+
+
+
+
+
+        return null;
+    }
+
+
+
 
     // Use this for initialization
     void Awake () {
@@ -25,7 +43,6 @@ public class WolfBehavoir : MonoBehaviour {
         m_detectionModule.Initialize(this);
         m_movementModule.Initialize(this);
         m_memoryModule.Initialize(this);
-        m_Behavior = GetComponent<SimpleBT>();
     }
 
     void OnEnable()
@@ -33,35 +50,10 @@ public class WolfBehavoir : MonoBehaviour {
         m_detectionModule.Initialize(this);
         m_movementModule.Initialize(this);
         m_memoryModule.Initialize(this);
-        m_Behavior = GetComponent<SimpleBT>();
     }
 	// Update is called once per frame
 	void Update () {
-	    if (m_Behavior.GetWolfState() != GameManager.WolfState.Sleep) {
-	        if (m_Target == null) {
-	            m_TargetTag = m_Behavior.GetTarget();
-	            if (m_TargetTag != null && m_TargetTag != m_currLocation) {
-	                Debug.Log("Target set to " + m_TargetTag);
-	                memorizedObjects = m_memoryModule.GetRememberedObjects();
-	                if (memorizedObjects.Count > 0) {
-	                    foreach (GameObject t in memorizedObjects) {
-	                        if (t.tag == m_TargetTag) {
-	                            //normally would add to list of correct targets but for now just take the first one
-	                            m_Target = t;
-	                            m_movementModule.Move(m_Target);
-	                            break;
-	                        }
-	                    }
-	                }
-	            }
-	        } else {
-	            if (m_movementModule.IsTargetReached()) {
-	                m_Target = null;
-	                m_currLocation = m_TargetTag;
-	                Debug.Log("Success");
-	            }
-	        }
-	    }
+	   
 	}
 
     void OnDisable()
