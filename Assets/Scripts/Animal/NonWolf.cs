e0b80526bf308ac55acef04a8cd4537764ebc9d7
Dimitar <uzunov.dimitar@yahoo.com>
2016-12-02 12:35:48 +0100

    Other stuff

--- Assets/Scripts/Animal/NonWolf.cs
+++ Assets/Scripts/Animal/NonWolf.cs
@@ -1,12 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class NonWolf : Animal {
 
-    public int size = 1; //animal size: 1 rabbit, 2 coyote, 3 elk
-    public float normalSpeed = 1f; //running speed
-    public int bravery = 0; //sets how many wolves (and taunts by wolves) are needed to make it run away instead of standing its ground
-    public float speedDrop = 0.5f; //how much speed it loses when running on the RoughTerrain area
+    public int Size = 1; //animal size: 1 rabbit, 2 coyote, 3 elk, 4 bear
+    public float NormalSpeed = 1f; //running speed
+    public int Bravery = 0; //sets how many wolves (and taunts by wolves) are needed to make it run away instead of standing its ground
+    public float SpeedDrop = 0.5f; //how much speed it loses when running on the RoughTerrain area
+    public int NearbyFriendly;
 
     private float fatigue;
     private bool wolvesAround;
@@ -14,8 +14,6 @@ public class NonWolf : Animal {
 	void Start () {
 	    fatigue = 0f;
 	}
-	
-	void Update () {
-	    //set a smaller behavior tree for it
-	}
+
+    //set a smaller behavior tree for it
 }
---
+++ Assets/Scripts/Wolf/WolfInfo.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class WolfInfo : MonoBehaviour {
+
+    public bool RecentlyAte;    //used for heirarchical eating
+    public bool RecentlyDrank;    //used for heirarchical eating
+    public bool IsMale;         //true - male, false - female
+    public bool IsHowling;      //is it currently howling
+
+    public int Type;            //0 - omega, 1 - beta, 2 alpha
+    public int PackNumb;        //0 - no pack, 1-2 packs
+    public int NearbyFriendly;  //number of friendly wolves nearby
+    public int Location;        //0 - neutral ground, 1 - friendly groun, 2 - enemy ground
+    public int CurrAge;         //0-2 pups; 3-6 young; 7-death old
+    public int MaxAge;          //between 11 and 15 years
+    public int Personality;     //0 - undefined yet, 1 - weak, 2 - average, 3 - strong
+
+    public string Task;
+    public string Target;
+
+    private void Start() {
+        if (Type == 0) Personality = 1;
+    }
+}
