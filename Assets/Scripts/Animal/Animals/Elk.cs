851f243832d5996fd8fd3c15a8e0bd0f0442b428
Dimitar <uzunov.dimitar@yahoo.com>
2016-12-08 05:46:22 +0200

    Initialization for the elk

--- Assets/Scripts/Animal/Animals/Elk.cs
+++ Assets/Scripts/Animal/Animals/Elk.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections.Generic;
 
 public class Elk : Animal {
 
@@ -8,14 +7,39 @@ public class Elk : Animal {
     public int Bravery = 0; //sets how many wolves (and taunts by wolves) are needed to make it run away instead of standing its ground
     public float SpeedDrop = 0.5f; //how much speed it loses when running on the RoughTerrain area
     private bool wolvesAround;
-    
-	void Start () {
+    private BaseRoutine m_behaviorTree;
+
+    void Start () {
+        m_strength = AnimalStrength.Strong;
+
+        m_gender = (Random.value >= .5) ? AnimalGender.Male : AnimalGender.Female;
+
+        switch ((int) (Random.value * 100) % 4)
+	    {
+            case 0: m_age = AnimalAge.Infant;
+	            break;
+            case 1: m_age = AnimalAge.YoungAdult;
+	            break;
+            case 2: m_age = AnimalAge.Adult;
+	            break;
+            case 3: m_age = AnimalAge.Elder;
+	            break;
+            default: Debug.Log("Impossible Elk age");
+	            break;
+        }
 
         for (int i = 0; i < (int)NeedType.NEED_COUNT; i++)
         {
             m_needs.SetNeed((NeedType)i, Random.Range(50, 101));
         }
+
+	    m_currentHealth = Random.Range(80, 101);
 	}
 
-    //set a smaller behavior tree for it
+    BaseRoutine CreateBehaviorTree() {
+        BehaviorTreeBuilder treeBuilder = new BehaviorTreeBuilder();
+
+
+        return treeBuilder;
+    }
 }
