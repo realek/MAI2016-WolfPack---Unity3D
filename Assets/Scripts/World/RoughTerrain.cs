618b1225d1115476648afbfb446a51814ef47b82
Dimitar <uzunov.dimitar@yahoo.com>
2016-12-18 23:37:34 +0200

    third spawn point & adjustment to terrainscript

--- Assets/Scripts/World/RoughTerrain.cs
+++ Assets/Scripts/World/RoughTerrain.cs
@@ -6,6 +6,7 @@ public class RoughTerrain : MonoBehaviour {
     private void OnCollisionEnter(Collision other) {
         if (other.gameObject.tag == "Prey") {
             other.gameObject.GetComponent<NavMeshAgent>().speed = 2f;
+            other.gameObject.GetComponent<Needs>().ModNeed(NeedType.Energy, -10f);
         }
     }
     private void OnCollisionExit(Collision other) {
