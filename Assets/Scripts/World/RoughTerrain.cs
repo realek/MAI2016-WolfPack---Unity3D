33a269089459c51b64359db443afc76c00aa98a9
Dimitar <uzunov.dimitar@yahoo.com>
2016-12-18 23:28:16 +0200

    Added rough terrain

---
+++ Assets/Scripts/World/RoughTerrain.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class RoughTerrain : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Prey") {
+            other.gameObject.GetComponent<NavMeshAgent>().speed = 2f;
+        }
+    }
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.tag == "Prey") {
+            other.gameObject.GetComponent<NavMeshAgent>().speed = 3.5f;
+        }
+    }
+}
